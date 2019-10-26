af3d2dfd38f58c19d42d251327284db2df789309
lucya3584 <lucya3584@yahoo.co.uk>
2019-10-26 16:06:41 +0100

    getting older, getting unhealthier DONE

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -18,6 +18,16 @@ it('has an initial age of 0', () => {
     expect(pet.age).toEqual(0);
     });
 
+it('has an initial hunger of 0', () => {
+    const pet = new Pet('Fido');
+    expect(pet.hunger).toEqual(0);
+});
+
+it('has an initial fitness of 10', () => {
+    const pet = new Pet('Fido');
+    expect(pet.fitness).toEqual(10);
+});
+
 
 describe('growUp', () => {
     it('increments the age by 1', () => {
@@ -27,5 +37,19 @@ describe('growUp', () => {
 
         expect(pet.age).toEqual(1);
         });
+
+    it('increments hunger by 5', () => {
+        const pet = new Pet('Fido')
+
+        pet.growUp();
+
+        expect(pet.hunger).toEqual(5);
+    });
+
+    it('decreases fitness by 3', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.fitness).toEqual(7);
+    });
 
 });
