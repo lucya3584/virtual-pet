62a003229aae62a1044b83a037b79a6464088cdc
lucya3584 <lucya3584@yahoo.co.uk>
2019-10-26 15:48:06 +0100

    added incremental year

--- src/pet.js
+++ src/pet.js
@@ -1,4 +1,15 @@
-function Pet() {}
+function Pet(name) {
+    this.name = name;
+    this.age = 0;
+}
+Pet.prototype.growUp = function() {
+    this.age += 1;
+};
+
+
+const pet = {
+    name: 'Fido'
+};
 
 
 module.exports = Pet;
